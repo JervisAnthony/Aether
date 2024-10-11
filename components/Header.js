4febddd283760d281ba2bb79499c6707011ec043
Jervis Anthony Saldanha <jervisaldanha@gmail.com>
2024-10-11 15:18:00 -0700

    Create Header.js
    creation of the Header for the app

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'; // Create this file for header-specific styles
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Aether</h1>
+      <p>Your window to the atmosphere</p>
+    </header>
+  );
+}
+
+export default Header;
