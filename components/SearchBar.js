1314b2c8d521774ac0bf67a6b6df5d98cb904647
Jervis Anthony Saldanha <jervisaldanha@gmail.com>
2024-10-11 15:22:22 -0700

    Update SearchBar.js

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './SearchBar.css'; // Create this file for search bar styles
+import './SearchBar.css'; 
 
 function SearchBar({ onSearch }) {
   const [location, setLocation] = useState('');
